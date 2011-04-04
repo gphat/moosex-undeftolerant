e5d1b64215dcded06a34e7c389958c79f33c3906
Jesse Luehrs <doy@tozt.net>
2011-04-03 21:49:57 -0500

    these version checks already happen in the exporter

--- lib/MooseX/UndefTolerant/Class.pm
+++ lib/MooseX/UndefTolerant/Class.pm
@@ -1,24 +1,22 @@
 package MooseX::UndefTolerant::Class;
 use Moose::Role;
 
-if ( $Moose::VERSION >= 1.9900 ) {
-        around('_inline_init_attr_from_constructor', sub {
-                my $orig = shift;
-                my $self = shift;
-                my ($attr, $idx) = @_;
+around _inline_init_attr_from_constructor => sub {
+    my $orig = shift;
+    my $self = shift;
+    my ($attr, $idx) = @_;
 
-                my @source = $self->$orig(@_);
+    my @source = $self->$orig(@_);
 
-                my $init_arg = $attr->init_arg;
+    my $init_arg = $attr->init_arg;
 
-                return
-                        "if ( exists \$params->{$init_arg} && defined \$params->{$init_arg} ) {",
-                                @source,
-                        '} else {',
-                               "delete \$params->{$init_arg};",
-                        '}';
-        });
-}
+    return
+        "if ( exists \$params->{$init_arg} && defined \$params->{$init_arg} ) {",
+            @source,
+        '} else {',
+            "delete \$params->{$init_arg};",
+        '}';
+};
 
 no Moose::Role;
 
